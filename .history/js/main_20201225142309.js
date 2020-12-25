4bd065b0b374cb308a211e48b0f675fb00f6e8b9
yamauchitomoya <42059799+plus-agc-service-yamauchi@users.noreply.github.com>
2020-12-25 17:02:05 +0900

    20201225
    update

---
+++ .history/js/main_20201225142309.js
@@ -0,0 +1,44 @@
+// rollover.js
+(function () {
+    function rolloverListener (event) {
+      var target, relatedTarget, reg, src;
+
+      target = event.target || event.toElement;
+      relatedTarget = event.relatedTarget || event.fromElement;
+
+      if (target.tagName === 'IMG') {
+        reg = /_off(?=\.\w+$)/;
+        src = target.src;
+
+        if (reg.test(src)) {
+          target.src = src.replace(reg, '_on');
+        }
+      }
+
+      if (relatedTarget && relatedTarget.tagName === 'IMG') {
+        reg = /_on(?=\.\w+$)/;
+        src = relatedTarget.src;
+
+        if (reg.test(src)) {
+          relatedTarget.src = src.replace(reg, '_off');
+        }
+      }
+    }
+
+    if (document.addEventListener) {
+      document.addEventListener('mouseover', rolloverListener, false);
+    } else if (document.attachEvent) {
+      document.attachEvent('onmouseover', rolloverListener);
+    }
+
+  })();
+
+  $(window).scroll(function(){
+    var top = $(".animation").offset().top; // ターゲットの位置取得
+    var position = top - $(window).height();  // 発火させたい位置
+    if($(window).scrollTop() > position){
+      $(".animation").css("display","block");
+    }else{
+      $(".animation").css("display","none");
+    }
+  })
