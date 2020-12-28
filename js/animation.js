4b7f38c12c5609c45e3394e971626f3411eab5f6
yamauchitomoya <42059799+plus-agc-service-yamauchi@users.noreply.github.com>
2020-12-28 21:09:01 +0900

    20201228
    update

--- js/animation.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function(){
-    var top = $(".animation").offset().top; // ターゲットの位置取得
-    var position = top - $(window).height();  // 発火させたい位置
-    if($(window).scrollTop() > position){
-      $(".animation").css("display","block");
-    }else{
-      $(".animation").css("display","none");
-    }
-  })
