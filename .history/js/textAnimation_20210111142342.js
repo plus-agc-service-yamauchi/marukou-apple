cabd72f28004774b520ff1a4dae3a22d91b034c4
yamauchitomoya <42059799+plus-agc-service-yamauchi@users.noreply.github.com>
2021-01-11 14:50:37 +0900

    20210111
    update

---
+++ .history/js/textAnimation_20210111142342.js
@@ -0,0 +1,24 @@
+// アニメーションさせたいクラス
+var container = $(".animationTxt");
+// アニメーションスピード
+var speed = 200;
+
+// テキストの間にスペースを入れます
+var content = $(container).html();
+var text = $.trim(content);
+var newHtml = "";
+
+// スペースで区切ったテキストを、テキストの数だけspanで囲む
+text.split("").forEach(function(v) {
+ newHtml += '<span>' + v + '</span>';
+});
+
+// spanで囲んだテキスト群をHTMLに戻す
+$(container).html(newHtml);
+
+// 1文字ずつ表示
+var txtNum = 0;
+setInterval(function() {
+  $(container).find('span').eq(txtNum).css({opacity: 1});
+ txtNum++
+}, speed);
