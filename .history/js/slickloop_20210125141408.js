cbf725651ac347b5b88b77e03d69410bfdecd60e
yamauchitomoya <42059799+plus-agc-service-yamauchi@users.noreply.github.com>
2021-01-25 15:06:30 +0900

    20210125
    update

---
+++ .history/js/slickloop_20210125141408.js
@@ -0,0 +1,31 @@
+const $slider = $("#js-slider");
+
+// 左右の透過の2周目ががたつく対応
+$slider.on("beforeChange", (event, slick, currentSlide, nextSlide) => {
+  $slider.find(".slick-slide").each((index, el) => {
+    const $this = $(el),
+      slickindex = $this.attr("data-slick-index");
+    if (nextSlide == slick.slideCount - 1 && currentSlide == 0) {
+      // 現在のスライドが最初のスライドでそこから最後のスライドに戻る場合
+      if (slickindex == "-1") {
+        // 最後のスライドに対してクラスを付与
+        $this.addClass("is-active-next");
+      } else {
+        // それ以外は削除
+        $this.removeClass("is-active-next");
+      }
+    } else if (nextSlide == 0) {
+      // 次のスライドが最初のスライドの場合
+      if (slickindex == slick.slideCount) {
+        // 最初のスライドに対してクラスを付与
+        $this.addClass("is-active-next");
+      } else {
+        // それ以外は削除
+        $this.removeClass("is-active-next");
+      }
+    } else {
+      // それ以外は削除
+      $this.removeClass("is-active-next");
+    }
+  });
+});
